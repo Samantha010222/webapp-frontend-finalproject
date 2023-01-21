1c201fb4bcde4dd1f19d20956fd8fb32efdcebb3
Samantha010222 <timbalsamanthaezra@gmail.com>
2023-01-21 19:38:37 +0800

    adding login and registration form

--- resources/views/homepage/includes/nav.blade.php
+++ resources/views/homepage/includes/nav.blade.php
@@ -1,12 +1,16 @@
        <!-- Navigation-->
-       <a class="menu-toggle rounded" href="#"><i class="fas fa-bars"></i></a>
+    <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top">
+        <div>
+        <li class="nav-item mx-0 mx-lg-1" ><a  class="btn btn-primary" href="/login">Login</a> <a  class="btn btn-primary" href="/register">register</a></li>   
+        <div>
+       <a class="menu-toggle rounded " href="#"><i class="fas fa-bars"></i></a>
         <nav id="sidebar-wrapper">
             <ul class="sidebar-nav">
-                <li class="sidebar-nav-item" ><a href="/login">Login</a></li>    
                 <li class="sidebar-nav-item"><a href="#page-top">Home</a></li>
                 <li class="sidebar-nav-item"><a href="#about">About</a></li>
                 <li class="sidebar-nav-item"><a href="#services">Services</a></li>
                 <li class="sidebar-nav-item"><a href="#portfolio">Portfolio</a></li>
                 <li class="sidebar-nav-item"><a href="#contact">Contact</a></li>
             </ul>
+        </nav>
         </nav>
